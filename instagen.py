2b29022806c569a1e6b458e5dd5e221a0593d9fd
Gianluca Truda <gianlucatruda@gmail.com>
2017-10-19 17:31:20 +0200

    Version 1.4 with ability to generate smaller 300x300 previews as well as full images 1080x1080 and more modular function.

--- instagen.py
+++ instagen.py
@@ -1,38 +1,58 @@
 #!/usr/bin/env python
 # Image Generator for Instagram
-# Version 1.3
+# Version 1.4
 
-version = "1.3"
+version = "1.4"
 
 import scipy.misc
 import numpy as np
 import random as rand
 import datetime as dt
 import math
+import sys
 
-# Image size as per Instagram 2017
-width = 1080
-height = 1080
-channels = 3
+# Image size as per Instagram 2017 = 1080x1080
+full_scale = 1080
+preview_scale = 300
 
-def getPoint(min, max):
-	miu = int((max-min)/2)
-	dev = int((max-min)/20)
-	return int(np.random.normal(miu, dev, 5)[0])
+def get_norm(x, y):
+	start = min(x,y)
+	end = max(x,y)
+	miu = int((end-start)/2)
+	dev = max(int((end-start)/20),1)
+	return int(np.random.normal(miu, dev, 1)[0])
 
-# Create an empty image
-img = np.zeros((height, width, channels), dtype=np.uint8)
+def get_values(x,y):
+	a = get_norm(0, 200)
+	b = get_norm(200, 255)
+	c = int((a/(b+1))*256)
+	return [a, b, c]
 
+def generate(width, height, type):
+	# Create an empty image
+	channels = 3
+	img = np.zeros((height, width, channels), dtype=np.uint8)
 
-for i in range(width*height):
-	if(i%width==0):
-		print('\r'+str(round(100*i/(width*height)))+'%',end='...')
-	a = int(math.sqrt(i))
-	b = getPoint(100, 200)
-	c = int((b/(a+1))*256)
-	img[getPoint(0, height)][a] = [100, b, c]
+	for x in range(width):
+		for y in range(height):
+			if(x%100==0):
+				print('\r'+str(round(100*x/width))+'%',end='...')
+			img[y][x] = get_values(x,y)
 
-# Save the image
-stamp = "{:%Y-%m-%d_%H-%M-%S}".format(dt.datetime.now())
-scipy.misc.imsave("output/gen_v"+version+"_"+stamp+".png", img)
-print("Image saved")
+	# Save the image
+	stamp = "{:%Y-%m-%d_%H-%M-%S}".format(dt.datetime.now())
+	scipy.misc.imsave("output/"+type+"_v"+version+"_"+stamp+".png", img)
+	print("\r100%...Image saved")
+
+def gen_preview():
+	generate(preview_scale, preview_scale, "prev")
+
+def gen_full():
+	generate(full_scale, full_scale, "gen")
+
+if __name__ == "__main__":
+	argv = sys.argv[1:]
+	if len(argv) >= 1:
+		gen_full()
+	else:
+		gen_preview()
